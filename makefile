8e5241fbaf7b84c288e8cc041da677e39de898e9
Replit user <>
2023-02-23 19:07:20 +0000

    Started interpreter

--- makefile
+++ makefile
@@ -1,12 +1,15 @@
 CC=g++
 CFLAGS="-Wno-cpp -ggdb -O0 -p"
-
+CFLAGS-debug="-Wno-cpp -ggdb -O0 -p"
 TARGET=knl
 
 DEPS=src/main.cpp
 
 all:
 	$(CC) $(CFLAGS) $(DEPS) -o $(TARGET)
+
+debug:
+	$(CC) $(CFLAGS) $(DEPS) -o $(TARGET)-debug
 
 clean:
 	$(RM) knl
--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include <string>
 #include <unordered_map>
 #include <vector>
@@ -39,30 +40,72 @@ public:
 };
 // ARRAYS ARE VARIABLE TYPE & 0x80
 
-typedef union {
-  long long integer;
-  double double_ver;
-} data;
+// typedef union {
+//   long long integer;
+//   double double_ver;
+// } data;
 
-typedef struct var {
+// typedef struct var {
+//   int type;
+//   data stored_value;
+// } var;
+// typedef struct arr {
+//   int type;
+//   void *ptr;
+//   int length;
+// } arr;
+
+// typedef union {
+//   var var_rep;
+//   arr arr_rep;
+// } variable;
+
+class Variable {
+public:
   int type;
-  data stored_value;
-} var;
-typedef struct arr {
-  int type;
+};
+class Integer : public Variable {
+public:
+  int data;
+	Integer(){
+		
+	}
+	Integer(int type, int data){
+		this->type = type;
+		this->data = data;
+	}
+};
+class Double : public Variable {
+public:
+  double data;
+	Double(int type, double data){
+		this->type = type;
+		this->data = data;
+	}
+};
+class String : public Variable {
+public:
+  string data;
+	String(int type, string data){
+		this->type = type;
+		this->data = data;
+	}
+};
+class Array : public Variable {
   void *ptr;
   int length;
-} arr;
-
-typedef union {
-  var var_rep;
-  arr arr_rep;
-} variable;
+	Array(int type, void *data, int length){
+		this->type = type;
+		this->ptr = data;
+		this->length = length;
+	}
+};
 
 class TokenType {
 public:
   enum types {
     KEYWORD,
+		END,
     IDENTIFIER,
     OPERATOR,
     STRING,
@@ -75,7 +118,9 @@ public:
   };
 };
 string token_type_str[] = {
-    "KEYWORD", "IDENTIFIER",
+    "KEYWORD", //done
+		"END",
+		"IDENTIFIER",//done
     "OPERATOR", // done
     "STRING",   // done
     // "WHITESPACE",
@@ -87,7 +132,7 @@ string token_type_str[] = {
 };
 typedef struct func {
   int type;
-  long long line_offset;
+  long long token_offset;
 } function;
 typedef struct tok {
   int token_type;
@@ -99,35 +144,38 @@ bool strong = false;
 bool no_warn = false;
 
 string keywords[]{
-    "void",
-    "num",
+    "void", // 0
+    "num",  // 1
     // "int",
     // "short",
     // "byte",
-    "full",
+    "full", // 2
     // "uint",
     // "ushort",
     // "ubyte",
-    "double",
+    "double", // 3
     // "float",
-    "char",
-    "string",
-    "func",
-    "label",
-    "module",
+    "char",   // 4
+    "string", // 5
+    "func",   // 6
+    "label",  // 7
+    "module", // 8
     // IO Keywords
-    "print",
-    "input",
-    "write",
-    "read",
+    "print", // 9
+    "input", // 10
+    "write", // 11
+    "read",  // 12
     // Control keywords
-    "if",
-    "elif",
-    "else",
-    "for",
-    "while",
-    "goto",
-    "end",
+    "if",    // 13
+    "elif",  // 14
+    "else",  // 15
+    "for",   // 16
+    "while", // 17
+    "goto",  // 18
+    "end",   // 19
+    "then",  // 20
+    "array", // 21
+		"sleep", // 22
 };
 
 const string CHARS =
@@ -145,21 +193,49 @@ public:
     std::cout << "SHELL_ERROR: SHELL HAS NOT BEEN IMPLEMENTED!\n";
   }
 };
+class Nums {
+public:
+	static int pow(int n, int exp){
+		if(exp == 0){
+			return 1;
+		}
+		int tmp = n;
+		for(int i = 0; i < exp; i++){
+			tmp *= n;
+		}
+		return tmp;
+	}
+	static int ATOI(string str){
+		int res = 0;
+		int mul = 1;
+		for(int i = str.length(); i > -1 ; i--){
+			if(i < 0){
+				break;
+			}
+			res += str[i] * mul;
+			mul *= 10;
+		}
+		return res;
+	}	
+};
 class Lang {
 private:
-  vector<unordered_map<string, variable>> vars;
+  vector<unordered_map<string, Variable *>> vars;
   int scope_num = 0;
+  int max_scope = 0;
   unordered_map<string, function> funcs;
   unordered_map<string, token> constants;
   vector<string> lines;
   vector<token> tokens;
+  unordered_map<string, Integer> ints();
   // string at the top of files is the file to contain the start of the program.
   // append lines of following files in order.
   vector<string> files;
 
 public:
   Lang() {
-    vars = vector<unordered_map<string, variable>>();
+    vars = vector<unordered_map<string, Variable *>>();
+    vars.push_back(unordered_map<string, Variable *>());
     funcs = unordered_map<string, function>();
     lines = vector<string>();
     tokens = vector<token>();
@@ -192,8 +268,8 @@ public:
     vector<string> errors = vector<string>();
     for (int i = 0; i < lines.size(); i++) {
       string line = lines[i];
-      for (int j = 0; j < line.size() - 1; j++) {
-        if (line[j] == ' ' || line[j] == '\n' || line[j] == '\t') {
+      for (int j = 0; j < line.size(); j++) {
+        if (line[j] == '\n' || line[j] == '\t') {
           // yes i know it's redundant
           continue;
         } else if (line[j] == '\"') {
@@ -210,6 +286,7 @@ public:
             }
             topush += line[j++];
           }
+          // j--;
           tokens.push_back(token{TokenType::STRING, topush});
           // std::cout << topush;
         } else if (line[j] == '#') {
@@ -257,11 +334,12 @@ public:
             while (CHARS.find(line[j]) != string::npos && j < line.size()) {
               id += line[j++];
             }
+            j--;
             token tmptok = tokenize_const((line.c_str() + j));
             if (tmptok.token_type == -1) {
               errors.push_back("KNL: Error: Tokenizer ran into an error while "
                                "parsing token: token not recognzied on line " +
-                               std::to_string(i) + "\nHERE: " + line);
+                               std::to_string(i + 1) + "\nHERE: " + line);
             }
             constants[id] = tmptok;
             tokens.push_back(token{TokenType::CONSTANT, tmptok.data});
@@ -277,6 +355,8 @@ public:
           while (NUMS.find(line[j]) != string::npos && j < line.size()) {
             num += line[j++];
           }
+          j--;
+          // std::cout << num;
           tokens.push_back(token{TokenType::NUMBER, num});
         } else if (line[j] == '/') {
           if (j >= line.size()) {
@@ -288,7 +368,7 @@ public:
             while (searching) {
               if (j >= line.size()) {
                 string err = "KNL: Error: Unterminated comment on line " +
-                             std::to_string(i) + "\nHERE: " + line;
+                             std::to_string(i + 1) + "\nHERE: " + line;
                 errors.push_back(err);
                 break;
               }
@@ -309,23 +389,8 @@ public:
           }
         } else if (OPERATORS.find(line[j]) != string::npos) {
           token tok = {TokenType::OPERATOR, ""};
-          if (j <= line.size() - 1) {
-            j = j <= 0 ? 0 : j - 1;
-            string sub = line.substr(j, 2);
-            if (sub == "--" || sub == "++" || sub == "==" || sub == ">=" ||
-                sub == "<=") {
-              tok.data = sub;
-              // j++;
-              tokens.push_back(tok);
-              j += 2;
-              continue;
-            }
-          }
-          j = j == 0 ? j : j + 1;
           tok.data += line[j];
           tokens.push_back(tok);
-          j++;
-
         } else if (CHARS.find(line[j]) != string::npos) {
           string str = (string) "";
           if (j > line.size()) {
@@ -334,19 +399,20 @@ public:
           while ((CHARS.find(line[j] != string::npos) && (j < line.size()))) {
 
             if (j >= line.size() || line[j] == ' ' || line[j] == '\n' ||
-                line[j] == '\t') {
+                line[j] == '\t' || CHARS.find(line[j]) == string::npos) {
               // yes, it's redunant. no, it doenst work without it :/
               break;
             }
             str += line[j++];
           }
+          // j--;
           token tok{};
           bool found = false;
           // hardcoded keywords.length, idk why its not working if it's not
           // hardcoded
-          for (int i = 0; i < 20; i++) {
-            string cmp = keywords[i];
-            if (keywords[i] == str) {
+          for (int k = 0; k < 23; k++) {
+            string cmp = keywords[k];
+            if (keywords[k] == str) {
               found = true;
             }
           }
@@ -367,7 +433,7 @@ public:
             if (j >= line.size()) {
               string err =
                   "KNL: Error: Unterminated Array Initializer on line " +
-                  std::to_string(i) + "\nHERE: " + line;
+                  std::to_string(i + 1) + "\nHERE: " + line;
               errors.push_back(err);
               break;
             }
@@ -375,7 +441,7 @@ public:
             if (tmptok.token_type == -1) {
               if (tmptok.data == "UNTERM_STRING") {
                 string err = "KNL: Error: Unterminated String on line " +
-                             std::to_string(i) + "\nHERE: " + line;
+                             std::to_string(i + 1) + "\nHERE: " + line;
                 errors.push_back(err);
                 break;
               }
@@ -385,7 +451,7 @@ public:
             } else if (tmptok.token_type != type) {
               errors.push_back("KNL: Error: Incompatible types in array "
                                "initializer on line " +
-                               std::to_string(i) + "\nHERE: " + line);
+                               std::to_string(i + 1) + "\nHERE: " + line);
             }
             bool instr = 0;
             bool tobreak = 0;
@@ -393,7 +459,7 @@ public:
               if (j >= line.size()) {
                 string err =
                     "KNL: Error: Missing ',' in Array Initializer on line " +
-                    std::to_string(i) + "\nHERE: " + line;
+                    std::to_string(i + 1) + "\nHERE: " + line;
                 errors.push_back(err);
                 break;
               }
@@ -416,10 +482,15 @@ public:
           // for(int k = 0; k < tmptoks.size();k++);
           token topush = token{TokenType::ARRAY, line.substr(start, start - j)};
           tokens.push_back(topush);
-        } else {
-          errors.push_back((string) "KNL: Error: Unexpected token \'" +
-                           line[j] + "\' on line " + std::to_string(i) +
-                           "\nHERE: " + line);
+        }
+				else if(line[j] == ';'){
+					token tok = token{TokenType::END, ";"};
+				}
+				else {
+          if (line[j] != ' ')
+            errors.push_back((string) "KNL: Error: Unexpected token \'" +
+                             line[j] + "\' on line " + std::to_string(i + 1) +
+                             "\nHERE: " + line);
         }
       }
     }
@@ -496,8 +567,149 @@ public:
     }
     return token{TokenType::NUMBER, "1"};
   }
+	//runner and runner functions
+	
+	
+	void sleep(int ticks){
+		int r = ticks * 10000;
+		for(int i = 0; i < r; i++);
+	}
+  void preprocess() {
+    for (int i = 0; i < tokens.size(); i++) {
+      if (tokens[i].token_type == TokenType::KEYWORD &&
+          tokens[i].data == "label") {
+        i++;
+        if (i > tokens.size()) {
+          std::cout << "Error: Label keyword with no label. Expected: "
+                       "TokenType::IDENTIFIER\n";
+        }
+
+        Integer *intt = new Integer();
+        intt->type = VarType::LABEL;
+        intt->data = i;
+
+        vars[0][tokens[i].data] = intt;
+        Integer *t = (Integer *)(vars[0][tokens[i].data]);
+        std::cout << "Label Data: " << vars[0][tokens[i].data]->type << " : "
+                  << t->data << " " << tokens[i].data << std::endl;
+      }
+    }
+  }
+  Variable *get_var(string varname) {
+    for (int i = scope_num; i >= 0; i--) {
+			if(i < 0){
+				break;
+			}
+      if (vars[i].count(varname)) {
+        return vars[i][varname];
+      }
+    }
+    return new Variable{-1};
+  }
+	int eval(vector<token> expr){
+		int op1 = 0;
+		int op2 = 0;
+		int op = 0;
+		//const string OPERATORS = "+-*/%&|^!~$@><=";
+		for(int i = 0; i < expr.size(); i++){
+			
+			if(expr[i].token_type == TokenType::IDENTIFIER){
+				// op1 = 
+			}
+			else if(expr[i].token_type == TokenType::NUMBER){
+				
+			}
+			else if(expr[i].token_type == TokenType::OPERATOR){
+				op = OPERATORS.find(expr[i].data);
+				std::cout << op;
+			}
+		}
+	}
+  void run() {
+    for (int i = 0; i < tokens.size(); i++) {
+      if (tokens[i].token_type == TokenType::COMMENT) {
+        continue;
+      } else if (tokens[i].token_type == TokenType::KEYWORD) {
+        if (tokens[i].data == "print") {
+          if (i >= tokens.size()) {
+            std::cout << "Error: builtin 'print' called at EOF!\n";
+            exit(-1);
+          }
+          string data = "";
+          if (tokens[++i].token_type == TokenType::IDENTIFIER) {
+            Variable *ret = get_var(tokens[i].data);
+            if (ret->type == -1) {
+              std::cout << "Error: Undeclared Identifier '" << tokens[i].data
+                        << "'!\n";
+							data = "Error";
+              // exit(-1);
+            }
+            if(ret->type == VarType::STRING){
+							data = "string";
+						}
+						else{
+							data = "hi";
+							std::cout << "Error!";
+						}
+          } else if (tokens[i].token_type == TokenType::STRING ||
+                     tokens[i].token_type == TokenType::NUMBER) {
+            data = tokens[i].data;
+          }
+          std::cout << data << std::endl;
+        }
+        if (tokens[i].data == "goto") {
+          token tmptok = tokens[i + 1];
+					if( tmptok.token_type == TokenType::IDENTIFIER){
+						string label = tmptok.data;
+						Integer *intt = (Integer *)get_var(label);
+						i = intt->data;
+					}
+          else if(tmptok.token_type == TokenType::NUMBER){
+						i = Nums::ATOI(tmptok.data);
+					}
+        }
+				else if(tokens[i].data == "num"){
+					vars[scope_num][tokens[++i].data] = new Integer(VarType::INT64, 0);
+					vector<token>expr = vector<token>();
+					while(tokens[i].token_type != TokenType::END){
+						if(tokens[i].token_type == TokenType::END){
+							break;
+							//I KNOW ALL OF THIS IS REDUNDANT BUT IDK WHY IT DOESNT WORK WITHOUT IT
+						}
+						expr.push_back(tokens[i++]);
+					}
+					i--;
+					eval(expr);
+				}
+				else if(tokens[i].data == "sleep"){
+					if(i > tokens.size()){
+						std::cout << "Error! Sleep keyword without any time!\n";
+						exit(-1);
+					}
+					if(tokens[++i].token_type == TokenType::NUMBER){
+						int time = Nums::ATOI(tokens[++i].data);
+						sleep(time);
+					}
+				}
+				else if(tokens[i].data == "if"){
+					if(tokens[i].data != "if"){
+						std::cout << "Not equal" << std::endl;
+						exit(-1);
+					}
+					std::cout << tokens[i].data << std::endl;
+					vector<token> expr = vector<token>();
+					i++;
+					while(!(tokens[i].data == "then")){
+						expr.push_back(tokens[i++]);
+					}
+					
+					i--;
+				}
+      }
+    }
+  }
 };
-class Nums {};
+
 }; // namespace KN
 
 int main(int argc, char **argv) {
@@ -520,6 +732,8 @@ int main(int argc, char **argv) {
     runner->read_files();
     runner->tokenize();
     runner->debug_print_toks();
+    runner->preprocess();
+    runner->run();
     // std::cout << "KEYWORD: ";
     // for(int i= 0; i < 20; i++){
     // 	std::cout << keywords[i] << " DATA: ";
